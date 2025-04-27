128496e6d20717c134e67438be556b648b11a138
VIWAlter <127553550+VIWAlter@users.noreply.github.com>
2025-04-27 17:59:03 +0300

    Update Word.js

--- Word.js
+++ Word.js
@@ -9,3 +9,27 @@ helpButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const helpButton = document.getElementById('help-button');
+  const modal = document.getElementById('help-modal');
+  const closeButton = document.querySelector('.close-button');
+
+  helpButton.addEventListener('click', function() {
+      modal.style.display = 'block'; // Показать модальное окно
+  });
+
+  closeButton.addEventListener('click', function() {
+      modal.style.display = 'none'; // Скрыть модальное окно
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+          modal.style.display = 'none'; // Скрыть модальное окно при клике вне его
+      }
+  });
+});
